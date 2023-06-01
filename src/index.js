9a497c96aeff58eb4688d3316524cca31bb62ec5
JiroFg <mechcromancer@gmail.com>
2023-06-01 16:03:26 -0600

    corse

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import iucrsRouter from "./routes/iucrs.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import { PORT } from "./config/config.js";
 import doorRouter from "./routes/door.routes.js";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors())
 app.use(express.json());
 app.use("/users", usersRouter);
 app.use("/classrooms", crsRouter);
